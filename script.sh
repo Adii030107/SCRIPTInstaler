8db2ec87283c45081626ffbebe87f077b8efe10e
Adii030107 <87555191+Adii030107@users.noreply.github.com>
2021-07-18 12:29:41 +0700

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,135 @@
+#!/bin/sh
+clear
+# code warna
+biru="\033[1;96m"
+kuning="\033[1;93m"
+merah="\033[1;91m"
+hijau="\033[1;92m"
+abu2="\033[1;90m"
+ungu="\033[1;95m"
+figlet Welcome | lolcat
+sleep 1
+clear
+echo "${merah}GUNAKAN TOOLS DENGAN BIJAK"
+sleep 1
+echo "${merah}SEMOGA BERMANFAAT"
+sleep 1
+echo "${merah}JANGAN LUPA SUBSCRIBE CHANNEL ASMR"
+sleep 5
+xdg-open https://youtu.be/bIrx2Iv5gsA
+sleep 1
+clear
+figlet INSTALLER | lolcat
+sleep 1
+echo "${abu2}[${biru}×${abu2}] ${hijau}Author : ${kuning}M̶R̶A̶d̶i̶a̶r̶a̶a̶a̶-0̶8̶7    "
+echo "${abu2}[${biru}×${abu2}] ${hijau}Team   : ${kuning}Cyber Attacker "
+echo "${merah}==================================================="
+echo "${hijau}1. ${kuning}SPAMSms+CALL"
+echo "${biru}==================================================="
+echo "${hijau}2. ${kuning}SPAMSmsV1 MRAdiaraaa-087"
+echo "${ungu}==================================================="
+echo "${hijau}3. ${kuning}TAMPILAN + KALENDER"
+echo "${hijau}==================================================="
+echo "${hijau}4. ${kuning}TOMBOL KANAN KIRI"
+echo "${abu2}==================================================="
+echo "${hijau}5. ${kuning}6 Virus Brutal"
+echo "${merah}==================================================="
+echo "${hijau}6. ${kuning}Link VIRUS BVUG BRUTAL!"
+echo "${ungu}==================================================="
+echo "${hijau}7. ${kuning}Juzz Amma"
+echo "${biru}==================================================="
+echo "${hijau}8. ${kuning}BOBOL WIFI"
+echo "${hijau}==================================================="
+echo "${hijau}9. ${kuning}PHISING-all"
+echo "${merah}==================================================="
+echo "${kuning}10. ${merah}KELUAR!"
+echo "${abu2}x××××××××××××××××××××××××××××××××××××××××××××××××××x"
+read -p "Masukkan Pilihan ~> " pece
+if [ $pece = 1 ]
+then
+clear
+echo "${merah}SILAHKAN SALIN SCRIPT DI BAWAH INI"
+echo "${hijau}git clone https://github.com/H0N3S/SPAM-V2.0"
+echo "${hijau}cd SPAM-V2.0"
+echo "${hijau}python main.py"
+fi
+
+if [ $pece = 2 ]
+then
+clear
+echo "${merah}SILAHKAN SALIN SCRIPT DI BAWAH INI"
+echo "${hijau}git clone https://github.com/Adii030107/SpamSMSV1"
+echo "${hijau}cd SpamSMSV1"
+echo "${hijau}python x.py"
+fi
+
+if [ $pece = 3 ]
+then
+clear
+echo "${merah}SILAHKAN SALIN SCRIPT DI BAWAH INI"
+echo "${hijau}git clone https://github.com/4NK3R-PRODUCT1ON/T4MPILAN-v3"
+echo "${hijau}ls"
+echo "${hijau}cd T4MPILAN-v3"
+echo "${hijau}python2 run.py"
+fi
+
+if [ $pece = 4 ]
+then
+clear
+echo "${merah}SILAHKAN SALIN SCRIPT DI BAWAH INI"
+echo "${hijau}git clone https://github.com/MrVirusSpm-07/pelater"
+echo "${hijau}cd pelater"
+echo "${hijau}sh pemula.sh"
+fi
+
+if [ $pece = 5 ]
+then
+clear
+echo "${merah}SILAHKAN SALIN SCRIPT DI BAWAH INI"
+echo "${hijau}git clone https://github.com/novalattasya/6VIRUS"
+echo "${hijau}cd 6VIRUS"
+echo "${hijau}sh 6VIRUS.sh"
+fi
+
+if [ $pece = 6 ]
+then
+clear
+echo "${merah}SILAHKAN SALIN SCRIPT DI BAWAH INI"
+echo "${hijau}git clone https://github.com/MrVirusSpm-07/link-virus"
+echo "${hijau}cd link-virus"
+echo "${hijau}sh download.sh"
+fi
+
+if [ $pece = 7 ]
+then
+clear
+echo "${merah}SILAHKAN SALIN SCRIPT DI BAWAH INI"
+echo "${hijau}git clone https://github.com/MrVirusSpm-07/juz-amma"
+echo "${hijau}cd juz-amma"
+echo "${hijau}sh amma.sh"
+fi
+
+if [ $pece = 8 ]
+then
+clear
+echo "${merah}SILAHKAN SALIN SCRIPT DI BAWAH INI"
+echo "${hijau}git clone https://github.com/esc0rtd3w/wifi-hacker"
+echo "${hijau}cd wifi-hacker"
+echo "${hijau}chmod +x wifi-hacker.sh"
+echo "${hijau}./wifi-hacker.sh"
+fi
+
+if [ $pece = 9 ]
+then
+clear
+echo "${merah}SILAHKAN SALIN SCRIPT DI BAWAH INI"
+echo "${hijau}git clone https://github.com/reyspeed/phish-all"
+echo "${hijau}cd phis-all"
+echo "${hijau}bash croot.sh"
+fi
+
+if [ $pece = 10 ]
+then
+clear
+echo "${hijau}Makasih Udah Pake Script MRAdiaraa-087"
+fi
